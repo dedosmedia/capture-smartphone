49dac9f3995ae8aa70a6b2cb30e13f2a87a1f105
Björn Söderqvist <cybear@gmail.com>
2012-10-28 21:28:16 +0100

    Interval shouldn't be hard coded

--- public/javascripts/helpers/event2socket.js
+++ public/javascripts/helpers/event2socket.js
@@ -1,18 +1,19 @@
 define(['ws','helpers/sensors'],
 function(ws, sensors){
+  var interval = 200;
   console.log(ws);
   var lastEmits={};
-  function emitAt100msInterval(name, e){
+  function emitAtInterval(name, e){
     if(!lastEmits[name]) lastEmits[name] = 0;
     var now=new Date().getTime();
-    if(now >= lastEmits[name] + 200 ){
+    if(now >= lastEmits[name] + interval ){
       ws.emit('event', {"type":name, e:e});
       lastEmits[name]=now;
     }// otherwise drop event to avoid spam
   }
 
-  sensors.deviceorientation(emitAt100msInterval);
-  sensors.devicemotion(emitAt100msInterval);
-  sensors.geolocation(emitAt100msInterval);
+  sensors.deviceorientation(emitAtInterval);
+  sensors.devicemotion(emitAtInterval);
+  sensors.geolocation(emitAtInterval);
 
 });
